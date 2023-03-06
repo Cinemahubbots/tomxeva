925716b87b7e148ce68c5c2b912c202df68f29a8
Cinemahubbots <126318209+Cinemahubbots@users.noreply.github.com>
2023-03-06 11:22:49 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Cinemahubbots/tomxeva.git /EvaMaria
+  git clone https://github.com/Cinemahubbots/tomxeva.git /tomxeva
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /EvaMaria
+  git clone $UPSTREAM_REPO /tomxeva
 fi
-cd /EvaMaria
+cd /tomxeva
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
